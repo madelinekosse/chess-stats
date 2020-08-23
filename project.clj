2b17e884835f7894a3a1fe72f719dfb66df818c4
Anders Eriksson <aeri3@kth.se>
2020-08-23 02:37:25 +0200

    release 0.1.0

--- project.clj
+++ project.clj
@@ -16,7 +16,7 @@
 ;; You should have received a copy of the GNU Lesser General Public License
 ;; along with Magnus.  If not, see <https://www.gnu.org/licenses/>.
 
-(defproject magnus "0.1.0-SNAPSHOT"
+(defproject magnus "0.1.0"
   :description "A chess library"
   :url "https://github.com/Anders-E/Chess"
   :license {:name "LGPL-3.0-or-later"
